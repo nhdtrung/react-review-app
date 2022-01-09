75d94079c477add634f0f4e58ade5ce659f74469
Travis Nguyen <travisnguyenhoang@gmail.com>
2022-01-09 22:17:15 +0700

    Create a Context & Provider

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,22 +1,26 @@
 import { motion, AnimatePresense } from "framer-motion";
+import { useContext } from "react";
 import PropTypes from "prop-types";
 import React from "react";
 import FeedbackItem from "./FeedbackItem";
+import FeedbackContext from "../context/FeedbackContext";
 
-function FeedbackList({ feedback, handleDelete }) {
-  if (!feedback || feedback.length == 0) {
+function FeedbackList({ handleDelete }) {
+  const {feedback} = useContext(FeedbackContext)
+
+  if (!feedback || feedback.length === 0) {
     return "No feedback yet!!!";
   }
 
-  FeedbackList.propTypes = {
-    feedback: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.number.isRequired,
-        text: PropTypes.string.isRequired,
-        rating: PropTypes.number.isRequired,
-      })
-    ),
-  };
+  // FeedbackList.propTypes = {
+  //   feedback: PropTypes.arrayOf(
+  //     PropTypes.shape({
+  //       id: PropTypes.number.isRequired,
+  //       text: PropTypes.string.isRequired,
+  //       rating: PropTypes.number.isRequired,
+  //     })
+  //   ),
+  // };
 
   return (
     <div className="feedback-list">
